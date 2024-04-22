5eb4f8109b39dec73cb1e610242c6b12af4f85d4
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-04-22 09:32:25 -0700

    Re-do order of resume items and various small style changes

--- src/components/ResumeSection.tsx
+++ src/components/ResumeSection.tsx
@@ -2,118 +2,119 @@ import SectionHeader from "./template/SectionHeader";
 import ActionButton from "./template/ActionButton";
 
 export default function ResumeSection() {
-  const data = {
-    myResume: [
-      {
-        id: 9,
-        type: "experience",
-        startDate: "10.2023",
-        endDate: "Present",
-        company: "Freelance",
-        logo: "ev_old_logo.png",
-        jobTitle: "Full Stack Web Developer",
-        location: "San Francisco Bay Area, CA",
-        details:
-          "Specializing in React, Typescript, and Tailwind CSS for front-end development and Node.js, Express, GraphQL, MySQL, and MongoDB for back-end development. Continuously honing my skills and knowledge in other frameworks/libraries like Next.js, Astro, and Framer Motion.",
-      },
-      {
-        id: 8,
-        type: "education",
-        startDate: "04.2023",
-        endDate: "10.2023",
-        logo: "ucb_logo.svg",
-        institution: "UC Berkeley Extension",
-        program: "Full Stack Web Development Course",
-        location: "Online",
-        details:
-          "Intensive, 6-month, full-stack web development boot camp led by a senior full stack developer with 20+ years of experience. Project-driven curriculum, with 19 weekly challenges and 3 collaborative projects to build out our portfolio. Developed skills and experience with everything from the fundamentals of HTML, CSS, and JavaScript, to React, Node.js, MySQL, MongoDB, and GraphQL in order to make full stack MERN applications.",
-      },
-      {
-        id: 7,
-        type: "education",
-        startDate: "12.2022",
-        endDate: "12.2022",
-        logo: "eandm_logo.png",
-        institution: "E and M Training Course",
-        program: "Siemens TIA Portal Software Development Course",
-        location: "Fremont, CA",
-        details:
-          "Two week, instructor-led, hands-on course using TIA Portal software to integrate PLC, HMI, and Variable Frequency Drive (VFD) controls. Learned how to program in Ladder Logic, Function Block Diagrams, Sequential Function Charts, and Structured Text.",
-      },
-      {
-        id: 6,
-        type: "experience",
-        startDate: "09.2020",
-        endDate: "01.2024",
-        logo: "tesla_logo.svg",
-        company: "Tesla Motors Inc.",
-        jobTitle: "Automation Controls Technician",
-        location: "Fremont, CA",
-        details:
-          "The go-to technician for any task that needed technical expertise with electronics, PLC programming, automation, robotic systems, and any engineering department projects.",
-      },
-      {
-        id: 5,
-        type: "experience",
-        startDate: "10.2019",
-        endDate: "09.2020",
-        logo: "tesla_logo.svg",
-        company: "Tesla Motors Inc.",
-        jobTitle: "Equipment Maintenance Technician",
-        location: "Fremont, CA",
-        details:
-          "Adapted quickly to dozens of unfamiliar mechanical, electronic, and robotic systems, diagnosing and correcting numerous faults. Collaborated with engineering for successful equipment commissioning and problem analysis.",
-      },
-      {
-        id: 4,
-        type: "experience",
-        startDate: "12.2010",
-        endDate: "10.2018",
-        logo: "usn_logo.svg",
-        company: "United States Navy",
-        jobTitle: "Electronics Technician",
-        location: "Japan & San Diego, CA",
-        details:
-          "Led two of the largest shipboard work centers, consistently excelling in inspections (90%+ scores). Resolved 25+ radar system casualties and boosted electronic test equipment readiness by 15%. Trained 47 personnel with dedication.",
-      },
-      {
-        id: 3,
-        type: "education",
-        startDate: "12.2010",
-        endDate: "04.2016",
-        logo: "usn_logo.svg",
-        institution: "US Navy Training Schools",
-        program: "Electronics Technology Schools",
-        location: "Various Locations, USA",
-        details:
-          "Mastered seamanship fundamentals, electrical theory, and radio/radar electronics. Excelled in six equipment schools, specializing in air traffic control radar systems, and became the lead technician on both assigned ships.",
-      },
-      {
-        id: 2,
-        type: "experience",
-        startDate: "05.2005",
-        endDate: "12.2010",
-        logo: "ev_old_logo.png",
-        company: "Freelance",
-        jobTitle: "Front-End Web Developer",
-        location: "Atlanta Metro Area, GA",
-        details:
-          "Built and maintained multiple websites for small businesses and schools, leveraging web design and development skills in Dreamweaver, Photoshop, HTML, and CSS.  Created promotional content to drive user engagement.",
-      },
-      {
-        id: 1,
-        type: "education",
-        startDate: "08.2001",
-        endDate: "05.2005",
-        logo: "nhs_logo.png",
-        institution: "Norcross High School",
-        program: "High School Diploma",
-        location: "Norcross, GA",
-        details:
-          "Spearheaded the creation of the school website, rapidly learning front-end web design and development principles to lead a successful class project.",
-      },
-    ],
-  };
+  const experience = [
+    {
+      id: 5,
+      type: "experience",
+      startDate: "10.2023",
+      endDate: "Present",
+      company: "Freelance",
+      logo: "ev_old_logo.png",
+      jobTitle: "Full Stack Web Developer",
+      location: "San Francisco Bay Area, CA",
+      details:
+        "Specializing in React, Typescript, and Tailwind CSS for front-end development and Node.js, Express, GraphQL, MySQL, and MongoDB for back-end development. Continuously honing my skills and knowledge in other frameworks/libraries like Next.js, Astro, and Framer Motion.",
+    },
+    {
+      id: 4,
+      type: "experience",
+      startDate: "09.2020",
+      endDate: "01.2024",
+      logo: "tesla_logo.svg",
+      company: "Tesla Motors Inc.",
+      jobTitle: "Automation Controls Technician",
+      location: "Fremont, CA",
+      details:
+        "The go-to technician for any task that needed technical expertise with electronics, PLC programming, automation, robotic systems, and any engineering department projects.",
+    },
+    {
+      id: 3,
+      type: "experience",
+      startDate: "10.2019",
+      endDate: "09.2020",
+      logo: "tesla_logo.svg",
+      company: "Tesla Motors Inc.",
+      jobTitle: "Equipment Maintenance Technician",
+      location: "Fremont, CA",
+      details:
+        "Adapted quickly to dozens of unfamiliar mechanical, electronic, and robotic systems, diagnosing and correcting numerous faults. Collaborated with engineering for successful equipment commissioning and problem analysis.",
+    },
+    {
+      id: 2,
+      type: "experience",
+      startDate: "12.2010",
+      endDate: "10.2018",
+      logo: "usn_logo.svg",
+      company: "United States Navy",
+      jobTitle: "Electronics Technician",
+      location: "Japan & San Diego, CA",
+      details:
+        "Led two of the largest shipboard work centers, consistently excelling in inspections (90%+ scores). Resolved 25+ radar system casualties and boosted electronic test equipment readiness by 15%. Trained 47 personnel with dedication.",
+    },
+    {
+      id: 1,
+      type: "experience",
+      startDate: "05.2005",
+      endDate: "12.2010",
+      logo: "ev_old_logo.png",
+      company: "Freelance",
+      jobTitle: "Front-End Web Developer",
+      location: "Atlanta Metro Area, GA",
+      details:
+        "Built and maintained multiple websites for small businesses and schools, leveraging web design and development skills in Dreamweaver, Photoshop, HTML, and CSS.  Created promotional content to drive user engagement.",
+    },
+  ];
+
+  const education = [
+    {
+      id: 4,
+      type: "education",
+      startDate: "04.2023",
+      endDate: "10.2023",
+      logo: "ucb_logo.svg",
+      institution: "UC Berkeley Extension",
+      program: "Full Stack Web Development Course",
+      location: "Online",
+      details:
+        "Intensive, 6-month, full-stack web development boot camp led by a senior full stack developer with 20+ years of experience. Project-driven curriculum, with 19 weekly challenges and 3 collaborative projects to build out our portfolio. Developed skills and experience with everything from the fundamentals of HTML, CSS, and JavaScript, to React, Node.js, MySQL, MongoDB, and GraphQL in order to make full stack MERN applications.",
+    },
+    {
+      id: 3,
+      type: "education",
+      startDate: "12.2022",
+      endDate: "12.2022",
+      logo: "eandm_logo.png",
+      institution: "E and M Training Course",
+      program: "Siemens TIA Portal Software Development Course",
+      location: "Fremont, CA",
+      details:
+        "Two week, instructor-led, hands-on course using TIA Portal software to integrate PLC, HMI, and Variable Frequency Drive (VFD) controls. Learned how to program in Ladder Logic, Function Block Diagrams, Sequential Function Charts, and Structured Text.",
+    },
+    {
+      id: 2,
+      type: "education",
+      startDate: "12.2010",
+      endDate: "04.2016",
+      logo: "usn_logo.svg",
+      institution: "US Navy Training Schools",
+      program: "Electronics Technology Schools",
+      location: "Various Locations, USA",
+      details:
+        "Mastered seamanship fundamentals, electrical theory, and radio/radar electronics. Excelled in six equipment schools, specializing in air traffic control radar systems, and became the lead technician on both assigned ships.",
+    },
+    {
+      id: 1,
+      type: "education",
+      startDate: "08.2001",
+      endDate: "05.2005",
+      logo: "nhs_logo.png",
+      institution: "Norcross High School",
+      program: "High School Diploma",
+      location: "Norcross, GA",
+      details:
+        "Spearheaded the creation of the school website, rapidly learning front-end web design and development principles to lead a successful class project.",
+    },
+  ];
 
   const ResumeItem = ({
     startDate,
@@ -137,27 +138,25 @@ export default function ResumeSection() {
     details: string;
   }) => {
     return (
-      <div className="flex h-full gap-4 rounded-lg border border-white/15 bg-gradient-to-tl from-stone-800 to-stone-950 p-4">
+      <div className="flex h-full gap-4 rounded-lg border border-white/10 bg-gradient-to-tl from-stone-800 to-stone-950 p-4">
         <div className="flex-shrink-0 pt-1">
           <img src={logo} alt={jobTitle || program} className="h-max w-auto" />
         </div>
         <div>
-          <div className="flex flex-col gap-4">
-            <div className="">
-              <div className="flex gap-2 text-sm uppercase text-stone-400">
-                <span>{endDate}</span>
-                <span className="material-symbols-sharp">arrow_right</span>
-                <span>{startDate}</span>
-              </div>
-              <h3 className="text-balance text-lg font-bold text-stone-300">
-                {jobTitle || program}
-              </h3>
-              <div className="flex flex-col justify-between text-sm uppercase text-stone-500 lg:flex-row">
-                <h4>{company || institution}</h4>
-                <h5>{location}</h5>
-              </div>
+          <div>
+            <div className="flex gap-2 text-sm font-medium uppercase text-stone-400">
+              <span>{endDate}</span>
+              <span className="material-symbols-sharp">arrow_right</span>
+              <span>{startDate}</span>
             </div>
-            <p className="text-balance text-sm leading-relaxed text-stone-400">
+            <h3 className="text-balance text-lg font-extrabold text-stone-300">
+              {jobTitle || program}
+            </h3>
+            <div className="mb-4 flex flex-col justify-between space-y-2 text-sm uppercase text-stone-400 lg:flex-row">
+              <h4 className="font-light">{company || institution}</h4>
+              <h5>{location}</h5>
+            </div>
+            <p className="text-balance font-light leading-relaxed text-stone-400">
               {details}
             </p>
           </div>
@@ -166,14 +165,27 @@ export default function ResumeSection() {
     );
   };
 
-  const ResumeList = () => {
-    const { myResume } = data;
+  type ResumeItem = {
+    id: number;
+    type: string;
+    startDate: string;
+    endDate: string;
+    logo: string;
+    company?: string;
+    institution?: string;
+    jobTitle?: string;
+    program?: string;
+    location: string;
+    details: string;
+  };
 
+  const ResumeList = ({ list }: { list: ResumeItem[] }) => {
     return (
-      <div className="container mx-auto mb-32 grid gap-x-16 gap-y-8 md:grid-cols-2 px-4 md:px-0">
-        {myResume.map(
+      <div className="space-y-8 md:w-1/2">
+        {list.map(
           ({
             id,
+            type,
             startDate,
             endDate,
             logo,
@@ -183,14 +195,10 @@ export default function ResumeSection() {
             program,
             location,
             details,
-          }) => (
+          }: ResumeItem) => (
             <div
               key={id}
-              className={
-                id % 2 === 0
-                  ? "rounded-lg border border-white/10 bg-stone-900 p-2 md:translate-y-16"
-                  : "rounded-lg border border-white/10 bg-stone-900 p-2"
-              }
+              className={`rounded-lg border border-white/10 bg-stone-900/75 p-2 ${type === "education" ? "md:translate-y-16" : ""}`}
             >
               <ResumeItem
                 startDate={startDate}
@@ -221,8 +229,8 @@ export default function ResumeSection() {
     const { header, text1, text2, href } = buttonData;
 
     return (
-      <div className="mx-auto flex flex-col items-center justify-center border-y border-white/25 bg-gradient-to-t from-stone-900 to-10% py-64 px-4 md:px-0">
-        <h4 className="mb-8 text-balance text-center text-4xl font-bold text-stone-200">
+      <div className="mx-auto flex flex-col items-center justify-center border-y border-white/25 bg-gradient-to-t from-stone-900 to-10% px-4 py-64 md:px-0">
+        <h4 className="mb-8 max-w-[30ch] text-balance text-center text-4xl font-medium text-stone-300">
           {header}
         </h4>
         <div className="flex gap-8 md:gap-16">
@@ -236,7 +244,10 @@ export default function ResumeSection() {
   return (
     <section id="resume">
       <SectionHeader title="My Professional and Academic Journey" />
-      <ResumeList />
+      <div className="container mx-auto pb-64 flex flex-col gap-16 px-4 md:flex-row md:px-0">
+        <ResumeList list={experience} />
+        <ResumeList list={education} />
+      </div>
       <Download />
     </section>
   );
